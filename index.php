85b8ab2ba96dadf68e455793e690b1b920b52bca
NelsonOmoniyi <omoniyinelson6@gmail.com>
2023-10-08 07:32:02 +0100

    added email and edit index intro

--- index.php
+++ index.php
@@ -11,35 +11,17 @@ include("inc/header.php")
       <nav id="navbar" class="navbar">
         <ul>
           <li><a class="nav-link scrollto active" href="#hero">Home</a></li>
-          <!-- <li><a class="nav-link scrollto" href="#services">Services</a></li> -->
-          <li class="dropdown"><a href="#AI" class="nav-link scrollto"><span>AI Generators</span> <i class="bi bi-chevron-down"></i></a>
-            <ul>
-              <li><a href="#AI" class="nav-link scrollto" >AI Caption Generator</a></li>
-              <li><a href="#AI" class="nav-link scrollto" >AI Translator</a></li>
-              <li><a href="#AI" class="nav-link scrollto" >Outline Generator</a></li>
-              <li><a href="#AI" class="nav-link scrollto" >Unique Title Generator</a></li>
-            </ul>
-          </li>
           <li class="dropdown"><a href="#Web" class="nav-link scrollto"><span>Web Tools</span> <i class="bi bi-chevron-down"></i></a>
             <ul>
-              <li><a href="#Web" class="nav-link scrollto" >Favicon Generator</a></li> 
               <li><a href="#Web" class="nav-link scrollto" >Password Generator</a></li>
-              <li><a href="#Web" class="nav-link scrollto" >Program Policy Generator</a></li>
-              <li><a href="#Web" class="nav-link scrollto" >Terms and Condition Generator</a></li>
+              <li><a href="#Web" class="nav-link scrollto" >Privacy Policy Generator</a></li>
+              <li><a href="#Web" class="nav-link scrollto" >AI Writer</a></li>
+              <li><a href="#Web" class="nav-link scrollto" >Outline Generator</a></li>
             </ul>
           </li>
-          <li class="dropdown"><a href="#SEO" class="nav-link scrollto"><span>Digital Marketing & SEO Tools</span> <i class="bi bi-chevron-down"></i></a>
-            <ul>
-              <li><a href="#SEO" class="nav-link scrollto" >SEO Analysis</a></li>
-              <li><a href="#SEO" class="nav-link scrollto" >CPC Keywords</a></li>
-              <li><a href="#SEO" class="nav-link scrollto" >Keyword Search</a></li>
-              <li><a href="#SEO" class="nav-link scrollto" >Backlinks Checker</a></li>
-            </ul>
-          </li>
+          <li><a class="nav-link scrollto" href="#">Our Portfolio</a></li>
           <li><a class="nav-link scrollto" href="#about">About Us</a></li>
           <li><a class="nav-link scrollto" href="#contact">Contact Us</a></li>
-          <!-- <li><a class="nav-link scrollto" href="#team">Team</a></li> -->
-          <!-- <li><a class="getstarted scrollto" href="#">Login</a></li> -->
         </ul>
         <i class="bi bi-list mobile-nav-toggle"></i>
       </nav><!-- .navbar -->
@@ -54,9 +36,9 @@ include("inc/header.php")
       <div class="row">
         <div class="col-lg-6 d-flex flex-column justify-content-center pt-4 pt-lg-0 order-2 order-lg-1" data-aos="fade-up" data-aos-delay="200">
           <h1>Reliable Automated Digital Solutions</h1>
-          <h2>Looking For The Best Automated Digital Marketing Tools? We've Got You Covered!</h2>
+          <h2>Looking For The Best Automated Digital Tools? We've Got You Covered!</h2>
           <div class="d-flex justify-content-center justify-content-lg-start">
-            <a href="#services" class="btn-get-started scrollto">Get Started</a>
+            <a href="#Web" class="btn-get-started scrollto">Get Started</a>
           </div>
         </div>
         <div class="col-lg-6 order-1 order-lg-2 hero-img" data-aos="zoom-in" data-aos-delay="200">
@@ -77,10 +59,10 @@ include("inc/header.php")
           <div class="col-lg-7 d-flex flex-column justify-content-center align-items-stretch  order-2 order-lg-1">
 
             <div class="content">
-              <h3>What are <strong>Automated Digital Marketing Tools?</strong></h3>
+              <h3>What are <strong>Automated Digital Tools?</strong></h3>
               <p>
-                These include everything from social media platforms to analytics and content generation and management tools like AI Writer, Email Checker, Keyword Generator, e.t.c. <br>
-                Allowing marketers to create, test and measure the performance of their campaigns, digital marketing tools ensure that marketers can launch and test campaigns quickly and effectively.
+              Digital tools are online platforms or applications leveraged by businesses or individuals to perform quick and optimized functions that ordinarily could be difficult and time consuming. <br>
+                These include file handling to summary generation, content generation and management tools like AI Writer, Random Password Generator, Privacy Policy Generator e.t.c. 
               </p>
             </div>
           </div>
@@ -91,28 +73,21 @@ include("inc/header.php")
       </div>
     </section><!-- End Why Us Section -->
 
-    <!-- ======= AI Generators Section ======= -->
-    <section id="AI" class="services section-bg">
-      
+
+    <!-- ======= Web Tools Section ======= -->
+    <section id="Web" class="services section-bg">
+        
       <div class="container" data-aos="fade-up">
 
         <div class="section-title">
-          <h2>AI Generators</h2>
+          <h2>Web Tools</h2>
 
         <div class="row">
-          <div class="col-xl-3 col-md-6 d-flex align-items-stretch" data-aos="zoom-in" data-aos-delay="100">
-            <div class="icon-box">
-              <div class="icon"><i class="bx bxl-dribbble"></i></div>
-              <h4><a href="ai-writer.php">AI Writer</a></h4>
-              <p>Our AI Writer helps you generate content on any topic of your choice with few seconds!</p>
-            </div>
-          </div>
-
           <div class="col-xl-3 col-md-6 d-flex align-items-stretch mt-4 mt-md-0" data-aos="zoom-in" data-aos-delay="200">
             <div class="icon-box">
               <div class="icon"><i class="bx bx-file"></i></div>
-              <h4><a href="">AI Translator</a></h4>
-              <p>Are you finding it difficult to translate text from one language to another?</p>
+              <h4><a href="privacy.php">Privacy Policy Generator</a></h4>
+              <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore</p>
             </div>
           </div>
 
@@ -124,110 +99,24 @@ include("inc/header.php")
             </div>
           </div>
 
-          <div class="col-xl-3 col-md-6 d-flex align-items-stretch mt-4 mt-xl-0" data-aos="zoom-in" data-aos-delay="400">
-            <div class="icon-box">
-              <div class="icon"><i class="bx bx-layer"></i></div>
-              <h4><a href="">Bala Blue</a></h4>
-              <p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis</p>
-            </div>
-          </div>
-
-        </div>
-        </div>
-        <br>
-
-      </div>
-    </section><!-- AI Generators Section -->
-
-    <!-- ======= Web Tools Section ======= -->
-    <section id="Web" class="services section-bg">
-        
-      <div class="container" data-aos="fade-up">
-
-        <div class="section-title">
-          <h2>Web Tools</h2>
-
-        <div class="row">
           <div class="col-xl-3 col-md-6 d-flex align-items-stretch" data-aos="zoom-in" data-aos-delay="100">
             <div class="icon-box">
               <div class="icon"><i class="bx bxl-dribbble"></i></div>
-              <h4><a href="">Favicon Generator</a></h4>
-              <p>Voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi</p>
-            </div>
-          </div>
-
-          <div class="col-xl-3 col-md-6 d-flex align-items-stretch mt-4 mt-md-0" data-aos="zoom-in" data-aos-delay="200">
-            <div class="icon-box">
-              <div class="icon"><i class="bx bx-file"></i></div>
-              <h4><a href="privacy.php">Privacy Policy Generator</a></h4>
-              <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore</p>
-            </div>
-          </div>
-
-          <div class="col-xl-3 col-md-6 d-flex align-items-stretch mt-4 mt-xl-0" data-aos="zoom-in" data-aos-delay="300">
-            <div class="icon-box">
-              <div class="icon"><i class="bx bx-tachometer"></i></div>
-              <h4><a href="">Terms & Conditions Generator</a></h4>
-              <p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia</p>
+              <h4><a href="ai-writer.php">AI Writer</a></h4>
+              <p>Our AI Writer helps you generate content on any topic of your choice with few seconds!</p>
             </div>
           </div>
 
           <div class="col-xl-3 col-md-6 d-flex align-items-stretch mt-4 mt-xl-0" data-aos="zoom-in" data-aos-delay="400">
             <div class="icon-box">
               <div class="icon"><i class="bx bx-layer"></i></div>
-              <h4><a href="">Bala Blue</a></h4>
+              <h4><a href="">Outline Generator</a></h4>
               <p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis</p>
             </div>
           </div>
-
         </div>
-        <br>
       </div>
     </section><!-- Web Tools Section -->
-
-    <!-- ======= SEO Section ======= -->
-    <section id="SEO" class="services section-bg">
-        
-      <div class="container" data-aos="fade-up">
-
-        <div class="section-title">
-          <h2>Digital Marketing & SEO Tools</h2>
-
-        <div class="row">
-          <div class="col-xl-3 col-md-6 d-flex align-items-stretch" data-aos="zoom-in" data-aos-delay="100">
-            <div class="icon-box">
-              <div class="icon"><i class="bx bxl-dribbble"></i></div>
-              <h4><a href="">Keyword Search</a></h4>
-              <p>Voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi</p>
-            </div>
-          </div>
-
-          <div class="col-xl-3 col-md-6 d-flex align-items-stretch mt-4 mt-md-0" data-aos="zoom-in" data-aos-delay="200">
-            <div class="icon-box">
-              <div class="icon"><i class="bx bx-file"></i></div>
-              <h4><a href="">Backlinks Checker</a></h4>
-              <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore</p>
-            </div>
-          </div>
-
-          <div class="col-xl-3 col-md-6 d-flex align-items-stretch mt-4 mt-xl-0" data-aos="zoom-in" data-aos-delay="300">
-            <div class="icon-box">
-              <div class="icon"><i class="bx bx-tachometer"></i></div>
-              <h4><a href="">CPC Keywords</a></h4>
-              <p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia</p>
-            </div>
-          </div>
-
-          <div class="col-xl-3 col-md-6 d-flex align-items-stretch mt-4 mt-xl-0" data-aos="zoom-in" data-aos-delay="400">
-            <div class="icon-box">
-              <div class="icon"><i class="bx bx-layer"></i></div>
-              <h4><a href="">SEO Analysis</a></h4>
-              <p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis</p>
-            </div>
-          </div>
-        </div>
-      </div>
-    </section><!-- SEO Section -->
 
     <!-- ======= Team Section ======= -->
     <section id="team" class="team section-bg">
